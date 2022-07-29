a42de62db6247f6745464c1cb1b14b9fa25ce48c
samucar1411 <samuelcardenas1411@hotmail.com>
2022-07-28 23:11:40 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Run on development 
+
+### 1. Clone the repository
+### 2. Run this command
+```
+yarn install
+```
+### 3. Must have Nest CLI
+
+```
+npm i -g @nest/cli
+```
+### 4. Set the database
+```
+docker-compose up -d
+```
+
+## Stack used
+
+* MongoDB
+* Nest
---
+++ src/pokemon/pokemon.controller.ts
@@ -0,0 +1,44 @@
+import { Controller, 
+        Get, 
+        Post, 
+        Body, 
+        Patch, 
+        Param, 
+        Delete } from '@nestjs/common';
+
+        import { ParseMongoIdPipe } from 'src/common/pipes';
+        import { CreatePokemonDto, UpdatePokemonDto } from './dto';
+import { PokemonService } from './pokemon.service';
+
+@Controller('pokemon')
+export class PokemonController {
+  constructor(private readonly pokemonService: PokemonService) {}
+
+  @Post()
+  create(@Body() createPokemonDto: CreatePokemonDto) {
+    return this.pokemonService.create(createPokemonDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.pokemonService.findAll();
+  }
+
+  @Get(':term')
+  findOne(@Param('term') term: string) {
+    return this.pokemonService.findOne(term);
+  }
+
+  @Patch(':term')
+  update(
+    @Param('term') term: string, 
+    @Body() updatePokemonDto: UpdatePokemonDto
+  ) {
+    return this.pokemonService.update(term, updatePokemonDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseMongoIdPipe) id: string) {
+    return this.pokemonService.remove(id);
+  }
+}
